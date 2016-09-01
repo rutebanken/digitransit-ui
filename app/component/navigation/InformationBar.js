ee662fc11cc9de3aad4a342e8e1a96e5d1931f3c
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-09-01 15:29:32 +0200

    NRP-473 Use configured text for different languages for in the information bar

--- app/component/navigation/InformationBar.js
+++ app/component/navigation/InformationBar.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Icon from '../icon/icon';
+import config from '../../config';
+import connectToStores from 'fluxible-addons-react/connectToStores';
 
 class InformationBar extends Component {
 
+  static propTypes = {
+    messages: PropTypes.array.isRequired,
+    currentLanguage: React.PropTypes.string.isRequired,
+  };
+
   constructor(props) {
     super(props);
+    this.messages = config.aboutThisService;
     this.state = {
       visible: true,
     };
@@ -13,9 +21,11 @@ class InformationBar extends Component {
   render = () =>
     <div className="information-bar">
       <Icon id="information-bar-close-icon" img="icon-icon_close" />
-
+      <p>{this.messages[this.props.currentLanguage].about}</p>
+      <p>{this.messages[this.props.currentLanguage].cookies}</p>
     </div>
 
 }
-
-export default InformationBar;
+export default connectToStores(InformationBar, ['PreferencesStore'], (context) => ({
+  currentLanguage: context.getStore('PreferencesStore').getLanguage(),
+}));
